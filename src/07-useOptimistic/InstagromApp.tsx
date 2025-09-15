01154a4e79327717db07a5dd5ea361eff83f59e5
= <=>
2025-09-15 18:21:28 -0300

    feat: agregar la biblioteca 'sonner' y mejorar la gestión de comentarios en InstagromApp

--- src/07-useOptimistic/InstagromApp.tsx
+++ src/07-useOptimistic/InstagromApp.tsx
@@ -1,30 +1,63 @@
-import { useState } from "react";
-
+import { useOptimistic, useState, useTransition } from "react";
+import { toast } from "sonner";
 interface Comment {
   id: number;
   text: string;
   optimistic?: boolean;
 }
 
+let lastId = 2;
+
 export const InstagromApp = () => {
+  const [isPending, startTransition] = useTransition();
   const [comments, setComments] = useState<Comment[]>([
     { id: 1, text: "¡Gran foto!" },
     { id: 2, text: "Me encanta 🧡" },
   ]);
 
+  const [optimisticComments, addOptimisticComment] = useOptimistic(
+    comments,
+    (currentComments, newCommentText: string) => {
+      lastId++;
+      return [
+        ...currentComments,
+        {
+          id: lastId,
+          text: newCommentText,
+          optimistic: true,
+        },
+      ];
+    }
+  );
+
   const handleAddComment = async (formData: FormData) => {
     const messageText = formData.get("post-message") as string;
-    console.log("Nuevo comentario, " + messageText);
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    addOptimisticComment(messageText);
 
-    setComments((prev) => [
-      ...prev,
-      {
-        id: new Date().getTime(),
-        text: messageText,
-      },
-    ]);
+    startTransition(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      //   setComments((prev) => [
+      //     ...prev,
+      //     {
+      //       id: new Date().getTime(),
+      //       text: messageText,
+      //     },
+      //   ]);
+
+      // ! Este seria el codigo para revertir el proceso
+      setComments((prev) => prev);
+      toast('Error al agregar el comentario', {
+        description:'Intente nuevamente',
+        duration: 10_000,
+        position: 'top-right',
+        action: {
+            label: "cerrar",
+            onClick: () => toast.dismiss(),
+        }
+      })
+    });
   };
 
   return (
@@ -43,7 +76,7 @@ export const InstagromApp = () => {
 
       {/* Comentarios */}
       <ul className="flex flex-col items-start justify-center bg-gray-300 w-[500px] p-4">
-        {comments.map((comment) => (
+        {optimisticComments.map((comment) => (
           <li key={comment.id} className="flex items-center gap-2 mb-2">
             <div className="bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center">
               <span className="text-white text-center">A</span>
@@ -70,7 +103,7 @@ export const InstagromApp = () => {
         />
         <button
           type="submit"
-          disabled={false}
+          disabled={isPending}
           className="bg-blue-500 text-white p-2 rounded-md w-full"
         >
           Enviar
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 // import { ScrambleWords } from "./05-useReducer/Scramblewords";
 import "./index.css";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { MemoCounter } from "./06-memos/MemoCounter";
 import { InstagromApp } from "./07-useOptimistic/InstagromApp";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
